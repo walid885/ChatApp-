1c56b016c590620a3472dabb9f8c717d792ebf33
walid885 <bettaieb88walid@gmail.com>
2023-02-06 22:05:36 +0100

    22:05 commit

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Cam from '../img/cam.png'
+import add from '../img/add.png'
+import more from '../img/more.png'
 
 function Chat() {
   return (
-    <div className='chats'>
-      
+    <div className='chat'>
+      <div className="chatInfo">
+        <span>Jane </span>
+        <div className="chatIcons">
+        <img src={Cam} alt='camera logo'></img>
+        <img src={add} alt='Add logo'></img>  
+        <img src={more} alt='more logo'></img>
+        </div>
+      </div>
 
     </div>
   )
