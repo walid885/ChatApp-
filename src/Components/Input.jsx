44d6307c968a319bb3ea4496a7850cc26615ad0a
walid885 <bettaieb88walid@gmail.com>
2023-02-07 16:12:46 +0100

    the final design

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import attach from '../img/attach.png'
+import img from '../img/img.png'
 function Input() {
   return (
-    <div className='Input'>inputpanel</div>
+    <div className='Input'>
+      <input type="text" placeholder='Type Something ... '/>
+      <div className="send">
+        <img src={attach} alt="" />
+        <input type="file" style ={{display : "none"}} id="file" />
+        <label htmlFor='file'>
+          <img src={img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+
+    </div>
   )
 }
 
--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Message() {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+    <div className="messageInfo">
+    <img src='https://images.pexels.com/photos/2102415/pexels-photo-2102415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt="some random dude I found on the internet " />
+    <span>Just Now </span>
+ 
+    </div>
+    <div className="messageContent">
+    <p>Hello</p>
+   <img src="https://images.pexels.com/photos/4709294/pexels-photo-4709294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"  alt =" an other random dude I found on the Interrnet!"/> 
+    </div>
+    </div>
   )
 }
 
